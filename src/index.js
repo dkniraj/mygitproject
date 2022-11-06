e3ab76d76bddcb2e00b527253540c43464f194e7
dkniraj <nirajncd786@gmail.com>
2022-11-06 12:41:33 +0530

    myDateFunction

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+)
+
+mongodb+srv:(//Niraj1998:Siliguri40@cluster0.4rxxcvq.mongodb.net/Niraj-1998?retryWrites=true&w=majority",) {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
@@ -18,10 +20,26 @@ app.use (
     function (req, res, next) {
         console.log ("inside GLOBAL MW");
         next();
-  }
-  );
+  });
+  mongoose.connect("const assignmentMW= function (req, res, next) {
+    var currentdate = new Date(); 
+    var datetime =  currentdate.getDate() + " "
+                    + (currentdate.getMonth()+1)  + " " 
+                    + currentdate.getFullYear() + "  "  
+                    + currentdate.getHours() + ":"  
+                    + currentdate.getMinutes() + ":" 
+                    + currentdate.getSeconds();
+ 
+    let ip= req.ip
+    let url= req.originalUrl
+    console.log(`${datetime}  ${ip}  ${url}`)
+    next()    
+  )
+ 
+app.use( assignmentMW )
 
-app.use('/', route);
+
+  app.use('/', route);
 
 
 app.listen(process.env.PORT || 3000, function () {
